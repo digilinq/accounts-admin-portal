ff30644b614d1f7512917fba600b9b038e596bb1
Hossein Mohammadi <hossein.mohammadi@outlook.com>
2024-10-05 22:05:25 +0200

    Remove user

--- src/src/components/userList.js
+++ src/src/components/userList.js
@@ -1,19 +1,20 @@
 import React from "react";
 import UserRow from "./userRow";
 
-const UserList = ({ users }) => {
+const UserList = ({ users, remove }) => {
   return (
     <>
-      <table>
+      <table className="table table-hover">
         <thead>
           <tr>
             <th>ID</th>
             <th>Username</th>
             <th>Email</th>
+            <th>Options</th>
           </tr>
         </thead>
         <tbody>
-          {users.map(user => <UserRow user={user} />)}
+          {users.map(user => <UserRow user={user} onRemove={remove} />)}
         </tbody>
       </table>
     </>
--- src/src/components/userRow.js
+++ src/src/components/userRow.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-const UserRow = ({user}) => {
+const UserRow = ({user, onRemove}) => {
   return <tr>
     <td>{user.id}</td>
     <td>{user.username}</td>
     <td>{user.email}</td>
+    <td>
+        <button className="btn btn-danger" onClick={() => onRemove(user.id)}>Delete</button>
+    </td>
   </tr>
 }
 
