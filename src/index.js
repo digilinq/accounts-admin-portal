57e8cb25decb805132bb79d95c6a07f0be293c73
Hossein Mohammadi <hossein.mohammadi@outlook.com>
2024-10-08 13:36:43 +0200

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import UserContainer from './src/components/userContainer.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
